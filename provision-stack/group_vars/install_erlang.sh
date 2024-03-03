c0abca0adb788b0a2d70ad69efecfbc761eafbca
Tony <guzantony@gmail.com>
2024-03-03 17:29:49 +0200

    fixed script

--- provision-stack/group_vars/install_erlang.sh
+++ provision-stack/group_vars/install_erlang.sh
@@ -17,4 +17,27 @@ sudo apt update
 
 # Install Erlang
 sudo apt install erlang -y
+
+# Add RabbitMQ team's GPG key
+curl -1sLf "https://keys.openpgp.org/vks/v1/by-fingerprint/0A9AF2115F4687BD29803A206B73A36E6026DFCA" | sudo gpg --dearmor | sudo tee /usr/share/keyrings/com.rabbitmq.team.gpg > /dev/null
+
+# Add RabbitMQ Erlang GPG key
+curl -1sLf https://dl.cloudsmith.io/public/rabbitmq/rabbitmq-erlang/gpg.E495BB49CC4BBE5B.key | sudo gpg --dearmor | sudo tee /usr/share/keyrings/io.cloudsmith.rabbitmq.E495BB49CC4BBE5B.gpg > /dev/null
+
+# Add RabbitMQ Server GPG key
+curl -1sLf https://dl.cloudsmith.io/public/rabbitmq/rabbitmq-server/gpg.9F4587F226208342.key | sudo gpg --dearmor | sudo tee /usr/share/keyrings/io.cloudsmith.rabbitmq.9F4587F226208342.gpg > /dev/null
+
+# Create and edit RabbitMQ repository list file
+sudo bash -c 'cat <<EOF > /etc/apt/sources.list.d/rabbitmq.list
+deb [signed-by=/usr/share/keyrings/io.cloudsmith.rabbitmq.E495BB49CC4BBE5B.gpg] https://dl.cloudsmith.io/public/rabbitmq/rabbitmq-erlang/deb/ubuntu bionic main
+deb-src [signed-by=/usr/share/keyrings/io.cloudsmith.rabbitmq.E495BB49CC4BBE5B.gpg] https://dl.cloudsmith.io/public/rabbitmq/rabbitmq-erlang/deb/ubuntu bionic main
+deb [signed-by=/usr/share/keyrings/io.cloudsmith.rabbitmq.9F4587F226208342.gpg] https://dl.cloudsmith.io/public/rabbitmq/rabbitmq-server/deb/ubuntu bionic main
+deb-src [signed-by=/usr/share/keyrings/io.cloudsmith.rabbitmq.9F4587F226208342.gpg] https://dl.cloudsmith.io/public/rabbitmq/rabbitmq-server/deb/ubuntu bionic main
+EOF'
+
+# Update apt cache
+sudo apt update -y
+
+# Install RabbitMQ server
+sudo apt-get install rabbitmq-server -y --fix-missing
 ###
