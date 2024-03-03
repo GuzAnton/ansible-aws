f00e5e6312a041326318c40254292df7410bd9df
Tony <guzantony@gmail.com>
2024-03-03 17:08:13 +0200

    test pb

--- provision-stack/group_vars/install_rabbitmq.sh
+++ provision-stack/group_vars/install_rabbitmq.sh
@@ -1,19 +1,38 @@
 #!/bin/bash
 
-# Update and upgrade packages
-sudo apt update && sudo apt upgrade
+# Step 1: Install curl and gnupg
+sudo apt install curl gnupg -y
 
-# Install required packages
-sudo apt install curl gnupg
+# Step 2: Add RabbitMQ team's GPG key
+curl -1sLf "https://keys.openpgp.org/vks/v1/by-fingerprint/0A9AF2115F4687BD29803A206B73A36E6026DFCA" | sudo gpg --dearmor | sudo tee /usr/share/keyrings/com.rabbitmq.team.gpg > /dev/null
 
-# Add RabbitMQ GPG key
-curl -fsSL https://packages.rabbitmq.com/gpg | sudo apt-key add -
+# Step 3: Add RabbitMQ Erlang GPG key
+curl -1sLf https://dl.cloudsmith.io/public/rabbitmq/rabbitmq-erlang/gpg.E495BB49CC4BBE5B.key | sudo gpg --dearmor | sudo tee /usr/share/keyrings/io.cloudsmith.rabbitmq.E495BB49CC4BBE5B.gpg > /dev/null
 
-# Add RabbitMQ repository
-sudo add-apt-repository 'deb https://dl.bintray.com/rabbitmq/debian focal main'
+# Step 4: Add RabbitMQ Server GPG key
+curl -1sLf https://dl.cloudsmith.io/public/rabbitmq/rabbitmq-server/gpg.9F4587F226208342.key | sudo gpg --dearmor | sudo tee /usr/share/keyrings/io.cloudsmith.rabbitmq.9F4587F226208342.gpg > /dev/null
 
-# Update package list after adding repository
-sudo apt update
+# Step 5: Install apt-transport-https
+sudo apt install apt-transport-https -y
 
-# Install RabbitMQ server
-sudo apt install rabbitmq-server
+# Step 6: Create and edit RabbitMQ repository list file
+sudo bash -c 'cat <<EOF > /etc/apt/sources.list.d/rabbitmq.list
+deb [signed-by=/usr/share/keyrings/io.cloudsmith.rabbitmq.E495BB49CC4BBE5B.gpg] https://dl.cloudsmith.io/public/rabbitmq/rabbitmq-erlang/deb/ubuntu bionic main
+deb-src [signed-by=/usr/share/keyrings/io.cloudsmith.rabbitmq.E495BB49CC4BBE5B.gpg] https://dl.cloudsmith.io/public/rabbitmq/rabbitmq-erlang/deb/ubuntu bionic main
+deb [signed-by=/usr/share/keyrings/io.cloudsmith.rabbitmq.9F4587F226208342.gpg] https://dl.cloudsmith.io/public/rabbitmq/rabbitmq-server/deb/ubuntu bionic main
+deb-src [signed-by=/usr/share/keyrings/io.cloudsmith.rabbitmq.9F4587F226208342.gpg] https://dl.cloudsmith.io/public/rabbitmq/rabbitmq-server/deb/ubuntu bionic main
+EOF'
+
+# Step 7: Update apt cache
+sudo apt update -y
+
+# Step 8: Install Erlang packages
+sudo apt install -y erlang-base \
+erlang-asn1 erlang-crypto erlang-eldap erlang-ftp erlang-inets \
+erlang-mnesia erlang-os-mon erlang-parsetools erlang-public-key \
+erlang-runtime-tools erlang-snmp erlang-ssl \
+erlang-syntax-tools erlang-tftp erlang-tools erlang-xmerl
+
+# Step 9: Install RabbitMQ server
+sudo apt-get install rabbitmq-server -y --fix-missing
+
